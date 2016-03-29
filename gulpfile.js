5cffa31492949c551dda4ae0fd846ff5bd145aaf
Maria Novokovich <kataskin@yandex.ru>
2016-03-29 23:51:59 +0300

    Сборка

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var clean = require('gulp-clean');
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -18,15 +23,60 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]})
+      ]}),
+      mqpacker({
+        sort: true
+        })
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.reload({stream: true}));
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
+
+//  .pipe(server.reload({stream: true}));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif,svg}")
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true
+    }))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("fonts", function() {
+    gulp.src("fonts/**/*.{woff,woff2}")
+    .pipe(gulp.dest("./build/fonts"));
+});
+
+gulp.task("script", function() {
+    gulp.src("js/**/*.js")
+    .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("html", function() {
+    gulp.src("*.html")
+    .pipe(gulp.dest("./build"));
+});
+
+gulp.task("clean", function () {
+  return gulp.src("./build", {read: false})
+    .pipe(clean());
+});
+
+gulp.task("build", [
+  "clean",
+  "fonts",
+  "html",
+  "script",
+  "style",
+  "images"
+]);
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "./build",
     notify: false,
     open: true,
     ui: false
